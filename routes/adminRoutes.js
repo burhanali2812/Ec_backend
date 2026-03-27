f86c0022b59ba48b22cc73b72cc8f4719a910f26
burhanali2812 <syedburhanali830@gmail.com>
2026-03-28 00:56:40 +0500

    add background2

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -24,5 +24,27 @@ router.post("/signUp", async(req,res)=>{
     }
 });
 
+router.post("/login", async(req,res)=>{
+    const {email, password} = req.body;
+    try {
+        // Check if admin exists
+        const admin = await Admin.findOne({ email });
+        if (!admin) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Check password     
+           const isMatch = await bcrypt.compare(password, admin.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Create and return JWT token
+        const payload = { adminId: admin._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "3h" });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 
 module.exports = router;
