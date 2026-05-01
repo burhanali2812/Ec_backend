af32dab0e4050c20983c0d4ac91285f7704a2825
burhanali2812 <syedburhanali830@gmail.com>
2026-05-01 15:01:37 +0500

    add get courses for teacher

--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -463,7 +463,7 @@ router.put("/payStudentFee/:feeId", authMiddleWare, async (req, res) => {
 });
 router.get("/getStudentFee/:studentId", authMiddleWare, async (req, res) => {
   const { studentId } = req.params;
-  let { month, feeFetchType } = req.query;
+
 
   try {
     const registration = await Registration.findOne({ student: studentId });
@@ -474,23 +474,14 @@ router.get("/getStudentFee/:studentId", authMiddleWare, async (req, res) => {
         success: false,
       });
     }
-    console.log(`Fetching fees for student ${studentId} with feeFetchType: ${feeFetchType} and month: ${month}`);
 
-    // If month parameter is not provided, use current month
-    if (!month) {
-      const now = new Date();
-      const year = now.getFullYear();
-      const monthNum = String(now.getMonth() + 1).padStart(2, "0");
-      month = `${year}-${monthNum}`;
-    }
+ 
 
     let query = {
       registration: registration._id,
-      month: month,
+ 
     };
-    if (feeFetchType === "all") {
-      delete query.month; // Remove month filter to fetch all fees for the student
-    }
+   
 
     const fees = await StudentFee.find(query).sort({ createdAt: -1 });
 
