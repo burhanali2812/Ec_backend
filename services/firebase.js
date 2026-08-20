a5a19764d10a0b647846c4481faa83e41bc6e2d5
burhanali2812 <syedburhanali830@gmail.com>
2026-08-21 04:50:36 +0500

    set env file

--- services/firebase.js
+++ services/firebase.js
@@ -1,12 +1,14 @@
 const { cert, getApps, initializeApp } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
-const serviceAccount = require("../credentials/firebase-service-account.json");
-
 const app =
   getApps().length === 0
     ? initializeApp({
-        credential: cert(serviceAccount),
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
       })
     : getApps()[0];
 
