711ebe0dba4be3cbd541c25a03b996a6d6262b25
burhanali2812 <syedburhanali830@gmail.com>
2026-08-21 05:12:24 +0500

    Fix Firebase production configuration conver base 64

--- services/firebase.js
+++ services/firebase.js
@@ -2,9 +2,19 @@ const { cert, getApps, initializeApp } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
 if (!getApps().length) {
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64
-  );
+  const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+  if (!base64) {
+    throw new Error(
+      "❌ FIREBASE_SERVICE_ACCOUNT_BASE64 is missing"
+    );
+  }
+
+  // Decode Base64 → JSON string
+  const json = Buffer.from(base64, "base64").toString("utf8");
+
+  // JSON string → JavaScript object
+  const serviceAccount = JSON.parse(json);
 
   initializeApp({
     credential: cert(serviceAccount),
