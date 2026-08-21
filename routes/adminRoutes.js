a997df7bcec4da7423178423098d9f8f7856a639
burhanali2812 <syedburhanali830@gmail.com>
2026-08-21 05:25:45 +0500

    Fix Firebase production configuration conver base 64

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,24 +7,24 @@ const TeacherReview = require("../modals/TeacherReviews");
 const router = express.Router();
 
 
-router.post("/signUp", async(req,res)=>{
-    const {email, password} = req.body;
-    try {
-        // Check if admin already exists
-        let admin = await Admin.findOne({ email });
-        if (admin) {
-            return res.status(400).json({ message: "Admin already exists" });
-        }   
-        // Create new admin
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        admin = new Admin({ email, password: hashedPassword  });
-        await admin.save();
-        res.status(201).json({ message: "Admin created successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error" });
-    }
-});
+// router.post("/signUp", async(req,res)=>{
+//     const {email, password} = req.body;
+//     try {
+//         // Check if admin already exists
+//         let admin = await Admin.findOne({ email });
+//         if (admin) {
+//             return res.status(400).json({ message: "Admin already exists" });
+//         }   
+//         // Create new admin
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
+//         admin = new Admin({ email, password: hashedPassword  });
+//         await admin.save();
+//         res.status(201).json({ message: "Admin created successfully" });
+//     } catch (error) {
+//         res.status(500).json({ message: "Server error" });
+//     }
+// });
 
 router.post("/login", async(req,res)=>{
     const {email, password} = req.body;
